a81ef1cae269b1b199a6e145369e6a3b72b7ee53
MarlonLLaguento <89112842+MarlonLLaguento@users.noreply.github.com>
2024-11-09 22:07:15 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,27 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
-	// Cargar el dataset completo
-	movieMap, err := loadDataset("ml_10m/movies.dat", "ml_10m/ratings_copia.dat")
+	// Cargar los datos de películas y calificaciones
+	movies, err := loadDataset("ml_10m/movies.dat", "ml_10m/ratings.dat")
 	if err != nil {
 		log.Fatalf("Error cargando el dataset: %v\n", err)
 	}
 
-	// Convertir el mapa de películas en un slice de películas
-	movies := make([]Movie, 0, len(movieMap))
-	for _, movie := range movieMap {
-		movies = append(movies, movie)
+	// Solicitar al usuario un ID de película
+	fmt.Print("Por favor, ingresa el ID de la película: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
+
+	// Convertir el input a un número entero
+	movieID, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("ID de película inválido.")
+		return
 	}
 
-	// Probar el sistema con diferentes movieIDs
-	testMovieIDs := []int{1193, 260, 589, 2571, 480, 527}
-	for _, movieID := range testMovieIDs {
-		fmt.Printf("\nProbando el sistema de recomendación para la película ID %d:\n", movieID)
-		startClient(movieID, movies)
+	// Buscar el nombre de la película y generar recomendaciones
+	var targetMovie *Movie
+	for _, movie := range movies {
+		if movie.ID == movieID {
+			targetMovie = &movie
+			break
+		}
 	}
+
+	if targetMovie == nil {
+		fmt.Printf("Película con ID %d no encontrada.\n", movieID)
+		return
+	}
+
+	fmt.Printf("Película seleccionada: %s\n", targetMovie.Title)
+	recommendations := generateItemBasedRecommendations(*targetMovie, movies)
+	fmt.Printf("Recomendaciones para la película %d - %s: %v\n", targetMovie.ID, targetMovie.Title, recommendations)
 }
+
